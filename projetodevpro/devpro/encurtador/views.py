814e93cbbef5b7a7c7a77287ce7ec11d6fb92583
Willian Oliveira Siqueira Cavalcante <woscavalcante@outlook.com>
2021-05-19 18:30:25 -0300

    First commit

---
+++ projetodevpro/devpro/encurtador/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+from devpro.encurtador.models import UrlRedirect
+
+
+def relatorios(requisicao, slug):
+    url_redirect = UrlRedirect.objects.get(slug=slug)
+    url_reduzida = requisicao.build_absolute_uri(f'/{slug}')
+    contexto = {
+        'reduce': url_redirect,
+        'url_reduzida': url_reduzida,
+    }
+    return render(requisicao, 'encurtador/relatorio.html', contexto)
+
+
+def redirecionar(requisicao, slug):
+    url_redirect = UrlRedirect.objects.get(slug=slug)
+    return redirect(url_redirect.destino)
